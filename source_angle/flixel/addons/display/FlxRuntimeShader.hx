c8237998cdb9f7fb1798ee37db9d8d7f0d9b4368
Homura <egzozu.be.bas@gmail.com>
2026-03-01 19:34:23 +0300

    Refactor GLSL version handling in shader processing

--- source_angle/flixel/addons/display/FlxRuntimeShader.hx
+++ source_angle/flixel/addons/display/FlxRuntimeShader.hx
@@ -50,6 +50,8 @@ class FlxRuntimeShader extends FlxGraphicsShader
     static final varyingKeyword:EReg = ~/\bvarying\s+(?:lowp|mediump|highp\s+)?([A-Za-z0-9_]+)\s+([A-Za-z0-9_]+)/g;
     static final texture2DKeyword:EReg = ~/\btexture2D\b/g;
     static final glFragColorKeyword:EReg = ~/\bgl_FragColor\b/g;
+	static final glVersionCleaner:EReg = ~/\b(\d+)\s*(?:core|es|compatibility)\b/g;
+	static final outFragColorKeyword:EReg = ~/\bout\s+vec4\s+openfl_FragColor\s*;\s*/g;
 
 	/**
 	 * Constructs a GLSL shader.
@@ -99,15 +101,15 @@ class FlxRuntimeShader extends FlxGraphicsShader
 	 */
 	@:noCompletion private function __processFragmentSource(input:String):String
 	{
-        switch (glVersion)
+        switch (glVersionCleaner.replace(glVersion, '$1'))
 		{
-            case "300 es", "310 es", "320 es":
+            case "300", "310", "320", "330", "400", "410", "420", "430", "440", "450", "460":
 				input = varyingKeyword.replace(input, "in $1 $2");
 				input = texture2DKeyword.replace(input, "texture");
 				input = glFragColorKeyword.replace(input, "openfl_FragColor");
 
 			default:
-				// we don't need do anything
+				input = outFragColorKeyword.replace(input, "");
 		}
 
 		var result = pragmaHeaderKeyword.replace(input, glFragmentHeaderRaw);
@@ -120,16 +122,16 @@ class FlxRuntimeShader extends FlxGraphicsShader
 	 */
 	@:noCompletion private function __processVertexSource(input:String):String
 	{
-        switch (glVersion)
+        switch (glVersionCleaner.replace(glVersion, '$1'))
 		{
-            case "300 es", "310 es", "320 es":
+           case "300", "310", "320", "330", "400", "410", "420", "430", "440", "450", "460":
                 input = attributeKeyword.replace(input, "in $1 $2");
 				input = varyingKeyword.replace(input, "out $1 $2");
 				input = texture2DKeyword.replace(input, "texture");
 				input = glFragColorKeyword.replace(input, "openfl_FragColor");
 
 			default:
-                // we don't need do anything
+                input = outFragColorKeyword.replace(input, "");
 		}
 
 		var result = pragmaHeaderKeyword.replace(input, glVertexHeaderRaw);
