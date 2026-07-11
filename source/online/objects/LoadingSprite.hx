ddc4686f7c1c2adbe0473b9583c5cb5774ee447e
Ethantobot <ethantobot7@gmail.com>
2026-07-11 12:54:27 +0200

    fix the crash when loading a song ig ?

--- source/online/objects/LoadingSprite.hx
+++ source/online/objects/LoadingSprite.hx
@@ -6,33 +6,54 @@ import openfl.geom.Point;
 import openfl.geom.Rectangle;
 
 class LoadingSprite extends FlxTypedGroup<FlxBasic> {
-    var loadBar:FlxSprite;
-    var tasksLength:Float = 0;
+var loadBar:FlxSprite;
+var tasksLength:Float = 0;
 
-	public function new(?tasksLength:Float, ?camera:FlxCamera) {
-        super();
-        
+public function new(?tasksLength:Float, ?camera:FlxCamera) {
+		super();
+		
 		var funkay = new FlxSprite();
 		var _funkayGraphic = Paths.image('funkay', null, false);
-		var funkayGraphic = (_funkayGraphic ?? FlxGraphic.fromRectangle(1675, 1083, FlxColor.BLACK)).bitmap;
-		funkay.makeGraphic(FlxG.width, FlxG.height, funkayGraphic.getPixel32(0, 0), true, "_funkay"); // kms
-		funkayGraphic.image.resize(Std.int(funkayGraphic.image.width * (FlxG.height / funkayGraphic.image.height)), FlxG.height);
-		funkay.graphic.bitmap.copyPixels(funkayGraphic, new Rectangle(0, 0, funkay.graphic.bitmap.width, funkay.graphic.bitmap.height),
-			new Point(FlxG.width / 2 - funkayGraphic.image.width / 2, 0));
+		
+		if (_funkayGraphic != null) {
+			var astcPath = Paths.getPath('images/funkay.astc', BINARY);
+			var isAstc = sys.FileSystem.exists(astcPath);
+
+			if (isAstc) {
+				funkay.loadGraphic(_funkayGraphic);
+				funkay.setGraphicSize(0, FlxG.height);
+				funkay.updateHitbox();
+				funkay.screenCenter(X);
+			} else {
+				var funkayGraphic = _funkayGraphic.bitmap;
+				if (funkayGraphic != null && funkayGraphic.image != null) {
+					funkay.makeGraphic(FlxG.width, FlxG.height, funkayGraphic.getPixel32(0, 0), true, "_funkay");
+					funkayGraphic.image.resize(Std.int(funkayGraphic.image.width * (FlxG.height / funkayGraphic.image.height)), FlxG.height);
+					funkay.graphic.bitmap.copyPixels(funkayGraphic, new Rectangle(0, 0, funkay.graphic.bitmap.width, funkay.graphic.bitmap.height),
+						new Point(FlxG.width / 2 - funkayGraphic.image.width / 2, 0));
+				} else {
+					funkay.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+				}
+			}
+		} else {
+			// Absolute fallback if the asset 'funkay' isn't found at all
+			funkay.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		}
+		
 		funkay.antialiasing = ClientPrefs.data.antialiasing;
-        add(funkay);
+		add(funkay);
 
 		loadBar = new FlxSprite(0, FlxG.height - 20).makeGraphic(FlxG.width, 10, 0xFFff16d2);
 		loadBar.scale.x = 0;
 		loadBar.visible = false;
 		loadBar.screenCenter(X);
-        add(loadBar);
+		add(loadBar);
 
 		if (camera != null)
 			cameras = [camera];
 
 		this.tasksLength = tasksLength;
-    }
+	}
 
     public function addProgress(remaining:Float) {
 		loadBar.scale.x += 0.5 * (FlxMath.remapToRange(remaining / tasksLength, 1, 0, 0, 1) - loadBar.scale.x);
